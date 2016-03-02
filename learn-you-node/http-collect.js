25b9c738f047b7261b8e0c53fc9500423642ba01
Evan Moore <etmoore@gmail.com>
2016-03-01 21:59:22 -0700

    handle error in callback

--- learn-you-node/http-collect.js
+++ learn-you-node/http-collect.js
@@ -5,6 +5,8 @@ var url = process.argv[2];
 
 http.get(url, (res) => {
   res.pipe(bl(function (err, data) {
+    if (err) return console.error(err);
+
     console.log(data.length);
     console.log(data.toString());
   }));
