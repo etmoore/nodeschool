cb37b9c7a928415af721212b76684fa09c3f8b09
Evan Moore <etmoore@gmail.com>
2016-03-13 13:04:13 -0700

    ask for a POST if method is not POST

--- stream-adventure/http-server.js
+++ stream-adventure/http-server.js
@@ -4,6 +4,8 @@ var through2 = require('through2');
 var server = http.createServer(function(req, res) {
   if (req.method === 'POST') {
     req.pipe(through2(write, end)).pipe(res);
+  } else {
+    res.end('method must be POST')
   }
 });
 server.listen(process.argv[2]);
