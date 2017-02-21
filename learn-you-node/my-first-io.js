673afefb0d435754943a78713b94f867690ad38d
Evan Moore <etmoore@gmail.com>
2017-02-21 10:52:43 -0700

    explain my-first-io.js

--- learn-you-node/my-first-io.js
+++ learn-you-node/my-first-io.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
+const fs = require('fs'); // require the fs module for working with filesystem
 
-var path = process.argv[2];
-var fileContents= fs.readFileSync(process.argv[2], 'utf8');
-var newLines = fileContents.split('\n').length - 1;
+var path = process.argv[2]; // get the path from the command line arguments
+var fileContents= fs.readFileSync(process.argv[2], 'utf8'); // read the file, assinging the contents to fileContents variable
+var newLines = fileContents.split('\n').length - 1; // split the file contents by new line into an array, then assign the length of that array - 1 to the variable newLines. We subtract 1 because there will be one less /n character than total number lines of text.
 
 console.log(newLines);
