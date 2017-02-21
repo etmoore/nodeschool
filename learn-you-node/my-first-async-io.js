9065653111d64af452bd20f0f635876e10ad1673
Evan Moore <etmoore@gmail.com>
2017-02-21 10:58:48 -0700

    explain my-first-async-io

--- learn-you-node/my-first-async-io.js
+++ learn-you-node/my-first-async-io.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-var path = process.argv[2];
-fs.readFile(path, 'utf8', function(err, data){
+var path = process.argv[2]; // grab the path from the command line arguments
+fs.readFile(path, 'utf8', function(err, data){ // asychronous because it's using a callback function to handle the error or data. Using the node idiom for function signatures: err as first argument, data as second.
   var newLineCount = data.split('\n').length - 1;
   console.log(newLineCount);
 });
