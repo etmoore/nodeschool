b733f4ff76ee20a148756a64cd78412ec39c768f
Evan Moore <etmoore@gmail.com>
2017-02-21 13:10:01 -0700

    explain http-collect

--- learn-you-node/http-collect.js
+++ learn-you-node/http-collect.js
@@ -1,13 +1,13 @@
 const http = require('http');
-const bl = require('bl');
+const bl = require('bl'); // import the Buffer List module (installed with npm)
 
 var url = process.argv[2];
 
 http.get(url, (res) => {
-  res.pipe(bl(function (err, data) {
-    if (err) return console.error(err);
+  res.pipe(bl(function (err, data) { // pipe the res stream to BL, which takes a callback.
+    if (err) return console.error(err); // if an error gets passed through, console log it.
 
-    console.log(data.length);
-    console.log(data.toString());
+    console.log(data.length); // console.log the length of the data.
+    console.log(data.toString()); // console.log the data in a string
   }));
 });
