89b538a1ba85cd288235c9557a7b5417d21d2f31
Evan Moore <etmoore@gmail.com>
2017-02-21 13:16:53 -0700

    commented juggling-async

--- learn-you-node/juggling-async.js
+++ learn-you-node/juggling-async.js
@@ -1,26 +1,26 @@
 var http = require('http');
-var results = [];
-var count = 0;
-var urls = process.argv.slice(2, process.argv.length);
+var results = []; // create array to hold results
+var count = 0; // initialize a counter to keep track of how many requests have come back
+var urls = process.argv.slice(2, process.argv.length); // get the urls from the command line arguments, store in array
 
-function printResults() {
+function printResults() { // this will be called when the http calls have completed. Prints the results.
   results.forEach((result)=> {
     console.log(result);
   });
 }
 
-urls.forEach((url, index)=> {
-  http.get(url, (res)=> {
-    var pageContent = '';
-    res.setEncoding('utf-8');
-    res.on('error', console.error);
-    res.on('data', (data)=> {
+urls.forEach((url, index)=> { // For each url...
+  http.get(url, (res)=> { // HTTP GET request, handing the response to a callback
+    var pageContent = ''; // initialize an empty string to hold the page content
+    res.setEncoding('utf-8'); // sets the encoding to be a string rather than a buffer
+    res.on('error', console.error); // if there's an error, hand it to console.error (function)
+    res.on('data', (data)=> { // when there's data, append it to pageContent
       pageContent += data;
     });
-    res.on('end', ()=> {
+    res.on('end', ()=> { // when the data floweth no more, add the page content to the results array, in the index of the http call (ensuring that it stays in order)
       results[index] = pageContent;
-      count++;
-      if (count === urls.length) {
+      count++; // increment the callback counter
+      if (count === urls.length) { // if there have been as many callbacks fired as urls provided, then log the results
         printResults();
       }
     });
