45615dde70dafbb3fbcefa25a42a635f7ae62a09
Evan Moore <etmoore@gmail.com>
2017-02-21 12:37:50 -0700

    commented http-client

--- learn-you-node/http-client.js
+++ learn-you-node/http-client.js
@@ -1,10 +1,10 @@
-var http = require('http');
+var http = require('http'); // require the http module
 
-var url = process.argv[2];
+var url = process.argv[2]; // get the url from the command line arguments
 
-http.get(url, (res) => {
+http.get(url, (res) => { // send GET HTTP request to the url, handing the response to the callback
   res.setEncoding('utf8');
-  res.on('data', (data) => {
+  res.on('data', (data) => { // when the response has data, console log that data
     console.log(data);
   });
 });
